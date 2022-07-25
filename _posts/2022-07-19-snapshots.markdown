c21f8b46bfeb1f4b102314dbce88464ded314ed2
Michal Med <michal.med.jr@gmail.com>
2022-07-25 10:21:19 +0200

    added documentation and snapshot in czech

---
+++ _posts/2022-07-19-snapshots.markdown
@@ -0,0 +1,9 @@
+---
+title:  "Snapshots"
+lang: en
+categories: Documentation
+#excerpt: "Open mic session about Jobs to be Done, includes presentation, video and further reading."
+---
+[Czech version]
+
+Since July 2022 TermIt supports creation of snapshots.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Snapshots",
+        "excerpt":"[Czech version]   Since July 2022 TermIt supports creation of snapshots.  ","categories": ["Documentation"],
+        "tags": [],
+        "url": "/documentation/snapshots/",
+        "teaser": null
+      },{
+        "title": "Revize",
+        "excerpt":"🇬🇧 English version Revize slouží k zakonzervování stavu slovníku v konkrétním čase. Revizi vytvoříte tlačítkem Vytvořit revizi v detailu slovníku. Revize slovníku je z pohledu ontologie zároveň slovníkem, který navíc obsahuje vazbu na na původní slovník a datum vytvoření revize. Při vytvoření revize slovníku jsou vytvořeny verze pojmů tohoto slovníku,...","categories": ["cs","Dokumentace"],
+        "tags": [],
+        "url": "/cs/dokumentace/snapshots/",
+        "teaser": null
+      }]
